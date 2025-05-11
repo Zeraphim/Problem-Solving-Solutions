16b466163b8ed2c8a86e1c8b23def3beb7f0cc56
Zeraphim <diamantejc87@gmail.com>
2025-05-12 00:44:07 +0800

    init

---
+++ Java/ProblemSolving/src/main/java/com/zeraphim/SpringAPICall/CodingBat/CodingBatApplication.java
@@ -0,0 +1,13 @@
+package com.zeraphim.SpringAPICall.CodingBat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingBatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingBatApplication.class, args);
+	}
+
+}

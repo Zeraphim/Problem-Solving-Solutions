458f072da32df7acf9d74df62de4e1a568379452
Zeraphim <diamantejc87@gmail.com>
2025-08-06 22:57:05 +0000

    Solve 1 CodingBat Java WarmUp1 Problem - makes10

---
+++ Java/ProblemSolving/src/main/java/com/zeraphim/SpringAPICall/CodingBat/a_WarmUp1/makes10.java
@@ -0,0 +1,33 @@
+package com.zeraphim.SpringAPICall.CodingBat.a_WarmUp1;
+
+/*
+
+Given 2 ints, a and b, return true if one if them is 10 or if their sum is 10.
+
+makes10(9, 10) → true
+makes10(9, 9) → false
+makes10(1, 9) → true
+
+ */
+
+public class makes10 {
+
+    public static void main(String[] args) {
+        System.out.println(makes10(9, 10));
+        System.out.println(makes10(9, 9));
+        System.out.println(makes10(1, 9));
+        System.out.println(makes10(10, 1));
+        System.out.println(makes10(10, 10));
+        System.out.println(makes10(8, 2));
+        System.out.println(makes10(8, 3));
+        System.out.println(makes10(10, 42));
+        System.out.println(makes10(12, -2));
+    }
+
+    public static boolean makes10(int a, int b) {
+        boolean oneIsTen = (a == 10) || (b == 10);
+        boolean sumIsTen = ((a + b) == 10);
+
+        return oneIsTen || sumIsTen;
+    }
+}

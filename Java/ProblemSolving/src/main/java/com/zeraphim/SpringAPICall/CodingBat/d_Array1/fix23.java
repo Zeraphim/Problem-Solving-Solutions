53ab7c3000f19e21fbe0d43b440654fbe1299d1c
Zeraphim <diamantejc87@gmail.com>
2025-11-06 23:10:07 +0800

    Solve 1 CodingBat Java Array1 Problem - fix23

---
+++ Java/ProblemSolving/src/main/java/com/zeraphim/SpringAPICall/CodingBat/d_Array1/fix23.java
@@ -0,0 +1,30 @@
+package com.zeraphim.SpringAPICall.CodingBat.d_Array1;
+
+/*
+
+Given an int array length 3, if there is a 2 in the array immediately followed by a 3, set the 3 element to 0. Return the changed array.
+
+fix23([1, 2, 3]) → [1, 2, 0]
+fix23([2, 3, 5]) → [2, 0, 5]
+fix23([1, 2, 1]) → [1, 2, 1]
+
+ */
+
+public class fix23 {
+    public static void main(String[] args) {
+        System.out.println(fix23(new int[]{1, 2, 3}));
+        System.out.println(fix23(new int[]{2, 3, 5}));
+        System.out.println(fix23(new int[]{1, 2, 1}));
+    }
+
+    private static int[] fix23(int[] nums) {
+
+        for (int i = 0; i <= nums.length - 1; i++) {
+            if(nums[i] == 2 && nums[i+1] == 3) {
+                nums[i+1] = 0;
+            }
+        }
+
+        return nums;
+    }
+}
